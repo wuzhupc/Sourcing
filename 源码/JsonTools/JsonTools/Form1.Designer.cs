21df87e8620514e0949f98858f67f56ed8acc14d
wuzhupc <wuzhupc@gmail.com>
2012-12-06 17:13:50 +0800

    JSON小工具

---
+++ 源码/JsonTools/JsonTools/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace JsonTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbtext = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbjson = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbtext
+            // 
+            this.tbtext.Location = new System.Drawing.Point(2, 12);
+            this.tbtext.Multiline = true;
+            this.tbtext.Name = "tbtext";
+            this.tbtext.Size = new System.Drawing.Size(630, 160);
+            this.tbtext.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "转为json";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "转为Text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbjson
+            // 
+            this.tbjson.Location = new System.Drawing.Point(2, 207);
+            this.tbjson.Multiline = true;
+            this.tbjson.Name = "tbjson";
+            this.tbjson.Size = new System.Drawing.Size(630, 200);
+            this.tbjson.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 411);
+            this.Controls.Add(this.tbjson);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbtext);
+            this.Name = "Form1";
+            this.Text = "JSONHTML";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbtext;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbjson;
+    }
+}
+
---
+++ 源码/JsonTools/JsonTools/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace JsonTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbtext.Text == "")
+            {
+                MessageBox.Show("请输入文本内容！");
+                tbtext.Focus();
+                return;
+            }
+            tbjson.Text = JsonConvert.SerializeObject(tbtext.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tbjson.Text == "")
+            {
+                MessageBox.Show("请输入JSOn文本内容！");
+                tbjson.Focus();
+                return;
+            }
+            tbtext.Text = (String)JsonConvert.DeserializeObject(tbjson.Text);
+        }
+    }
+}
